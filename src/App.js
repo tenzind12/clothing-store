971ff9a189b895f9a57ab7a7502864f9892af645
Tenzin Dekyong <dhontenzin@gmail.com>
2022-07-01 15:09:45 +0200

    signInComponent funcionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './components/sign-in/sign-in.component';
+import Authentication from './components/authentication/authentication.component';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils';
+import Button from '../button/button.component';
+import FormInput from '../form-input/form-input.component';
+import './sign-in-form.styles.scss';
+
+const SignInForm = () => {
+  // input states
+  const [formFields, setFormFields] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formFields;
+
+  // INPUT CHANGE HANDLER
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  // GOOGLE POP-UP SIGN IN
+  const signInUserWithGooglePopup = async () => {
+    const response = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(response.user);
+  };
+
+  // FORM SUBMIT HANDLER
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password);
+      console.log(response);
+      // clear form fields
+      setFormFields({ displayName: '', email: '', password: '', confirmPassword: '' });
+    } catch (error) {
+      switch (error.code) {
+        case 'auth/wrong-password':
+          alert('Incorrect Password');
+          break;
+        case 'auth/user-not-found':
+          alert('Email not correct');
+          break;
+        default:
+          console.log(error.code);
+      }
+    }
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account? Sign in with your email and password</h2>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" buttonType="google" onClick={signInUserWithGooglePopup}>
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
